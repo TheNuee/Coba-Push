aacc91d4e8b32b2a2c8967dc03647b937e9d9328
Rizky <Rizky@DESKTOP-HV8NOO1>
2018-09-12 12:54:15 +0700

    initial commit

--- src/calculator2/Calculator2.java
+++ src/calculator2/Calculator2.java
@@ -1,4 +1,3 @@
-
 package calculator2;
 
 import java.util.Scanner;
@@ -12,46 +11,44 @@ public class Calculator2 {
      */
     public static void main(String[] args) {
 
-       
         //Data
-        double bil1,bil2,hsl = 0;
+        double bil1, bil2, hsl = 0;
         char operator;
-         //Scanner imprt
+        //Scanner import
         Scanner sc = new Scanner(System.in);
-        
+
         //Start Prog.
         System.out.println("**** Kalkulator ****");
         System.out.println("----SETYA ELCOM----");
         System.out.println("#MUHAMMAD RIZKY A.H#");
         System.out.println("####################");
-        System.out.println("******N--C--M*******");
+        System.out.println("Tambah ( + ), Kurang ( - ), Kali ( * ), Bagi ( / ), Hasil Bagi ( % )");
         System.out.print("Bilangan Pertama : ");
         bil1 = sc.nextDouble();
         System.out.print("Operasi : ");
         operator = sc.next().charAt(0);
         System.out.print("Bilangan Kedua : ");
         bil2 = sc.nextDouble();
-        
-        if (operator=='+')
-        {hsl=bil1+bil2;
-        System.out.println("Hasil : " +bil1+ " + " +bil2+ " = " +hsl);}
-        else if (operator=='-')
-        {hsl=bil1-bil2;
-        System.out.println("Hasil : " +bil1+ " - " +bil2+ " = " +hsl);}
-        else if (operator=='*')
-        {hsl=bil1*bil2;
-        System.out.println("Hasil : " +bil1+ " * " +bil2+ " = " +hsl);}
-        else if (operator=='/')
-        { hsl=bil1/bil2;
-        System.out.println("Hasil : " +bil1+ " / " +bil2+ " = " +hsl);}
-        else if (operator=='%')
-        {hsl=bil1%bil2;
-        System.out.println("Hasil : " +bil1+ " % " +bil2+ " = " +hsl);}
-        else
-        {
+
+        if (operator == '+') {
+            hsl = bil1 + bil2;
+            System.out.println("Hasil : " + bil1 + " + " + bil2 + " = " + hsl);
+        } else if (operator == '-') {
+            hsl = bil1 - bil2;
+            System.out.println("Hasil : " + bil1 + " - " + bil2 + " = " + hsl);
+        } else if (operator == '*') {
+            hsl = bil1 * bil2;
+            System.out.println("Hasil : " + bil1 + " * " + bil2 + " = " + hsl);
+        } else if (operator == '/') {
+            hsl = bil1 / bil2;
+            System.out.println("Hasil : " + bil1 + " / " + bil2 + " = " + hsl);
+        } else if (operator == '%') {
+            hsl = bil1 % bil2;
+            System.out.println("Hasil : " + bil1 + " % " + bil2 + " = " + hsl);
+        } else {
             System.out.println("XX-I OPERATOR SALAH I-XX");
         }
-        
+
     }
-    
+
 }
